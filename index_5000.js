5b390a0131affd32ef364ffc9af7a112ca41bac4
marutimuthu <marutimuthu@gmail.com>
2021-02-25 18:42:53 +0530

    Isolated data and stats

--- index_5000.js
+++ index_5000.js
@@ -23,33 +23,33 @@ app.use('/charts/plugin', express.static(__dirname + '/node_modules/chartjs-plug
 app.use('/plugin', express.static(__dirname + '/node_modules/hammerjs/'));
 app.use('/css', express.static(__dirname + '/node_modules/tailwindcss/dist/'));
 app.use('/font', express.static(__dirname + '/node_modules/@fortawesome/fontawesome-free/'));
-app.use('/env', express.static(__dirname + '/html/'));
+app.use('/env', express.static(__dirname + '/html_/'));
 app.use('/favicon_io', express.static(__dirname + '/favicon_io/'));
 
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname + "/html/login.html"));
+    res.sendFile(path.join(__dirname + "/html_/login.html"));
 });
 
 app.get("/users", (req, res) => {
-    res.sendFile(path.join(__dirname + "/html/users.html"));
+    res.sendFile(path.join(__dirname + "/html_/users.html"));
 });
 
 app.get("/recipe", (req, res) => {
-    res.sendFile(path.join(__dirname + "/html/recipe.html"));
+    res.sendFile(path.join(__dirname + "/html_/recipe.html"));
 });
 
 app.get("/audit", (req, res) => {
-    res.sendFile(path.join(__dirname + "/html/audit.html"));
+    res.sendFile(path.join(__dirname + "/html_/audit.html"));
 });
 
 app.get("/alarm", (req, res) => {
-    res.sendFile(path.join(__dirname + "/html/alarm.html"));
+    res.sendFile(path.join(__dirname + "/html_/alarm.html"));
 });
 
 app.get("/dashboard", (req, res) => {
     res.header('Access-Control-Allow-Origin', '*');
-    res.sendFile(path.join(__dirname + "/html/index.html"));
+    res.sendFile(path.join(__dirname + "/html_/index.html"));
 });
 
 app.get("/login/logo", (req, res) => {
@@ -60,63 +60,63 @@ app.get("/login/image", (req, res) => {
 });
 
 app.get("/graphLHS", (req, res) => {
-    res.sendFile(path.join(__dirname + "/html/graphLHS.html"));
+    res.sendFile(path.join(__dirname + "/html_/graphLHS.html"));
 });
 
 app.get("/graphRHS", (req, res) => {
-    res.sendFile(path.join(__dirname + "/html/graphRHS.html"));
+    res.sendFile(path.join(__dirname + "/html_/graphRHS.html"));
 });
 
 app.get("/dido", (req, res) => {
-    res.sendFile(path.join(__dirname + "/html/dido.html"));
+    res.sendFile(path.join(__dirname + "/html_/dido.html"));
 });
 
 app.get("/do", (req, res) => {
-    res.sendFile(path.join(__dirname + "/html/do.html"));
+    res.sendFile(path.join(__dirname + "/html_/do.html"));
 });
 
 app.get("/io_maintanience", (req, res) => {
-    res.sendFile(path.join(__dirname + "/html/io_maintanience.html"));
+    res.sendFile(path.join(__dirname + "/html_/io_maintanience.html"));
 });
 
 app.get("/onboard", (req, res) => {
-    res.sendFile(path.join(__dirname + "/html/onboard.html"));
+    res.sendFile(path.join(__dirname + "/html_/onboard.html"));
 });
 
 app.get("/history", (req, res) => {
-    res.sendFile(path.join(__dirname + "/html/history.html"));
+    res.sendFile(path.join(__dirname + "/html_/history.html"));
 });
 
 app.get("/analytics", (req, res) => {
-    res.sendFile(path.join(__dirname + "/html/analytics.html"));
+    res.sendFile(path.join(__dirname + "/html_/analytics.html"));
 });
 
 app.get("/audit", (req, res) => {
-    res.sendFile(path.join(__dirname + "/html/audit.html"));
+    res.sendFile(path.join(__dirname + "/html_/audit.html"));
 });
 
 app.get("/settings", (req, res) => {
-    res.sendFile(path.join(__dirname + "/html/settings.html"));
+    res.sendFile(path.join(__dirname + "/html_/settings.html"));
 });
 
 app.get("/settings_recipie", (req, res) => {
-    res.sendFile(path.join(__dirname + "/html/settings_recipie.html"));
+    res.sendFile(path.join(__dirname + "/html_/settings_recipie.html"));
 });
 
 app.get("/settings_awc", (req, res) => {
-    res.sendFile(path.join(__dirname + "/html/settings_awc.html"));
+    res.sendFile(path.join(__dirname + "/html_/settings_awc.html"));
 });
 
 app.get("/settings_limit", (req, res) => {
-    res.sendFile(path.join(__dirname + "/html/settings_limit.html"));
+    res.sendFile(path.join(__dirname + "/html_/settings_limit.html"));
 });
 
 app.get("/settings_setup", (req, res) => {
-    res.sendFile(path.join(__dirname + "/html/settings_setup.html"));
+    res.sendFile(path.join(__dirname + "/html_/settings_setup.html"));
 });
 
 app.get("/settings_user", (req, res) => {
-    res.sendFile(path.join(__dirname + "/html/settings_user.html"));
+    res.sendFile(path.join(__dirname + "/html_/settings_user.html"));
 });
 
 app.get("/onboard/:namee/:machinee/:recepiee/:batchh", (req, res) => {
