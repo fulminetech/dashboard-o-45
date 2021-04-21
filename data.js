2a5a447efdc1f90cfb4b927de20ca70fddb9eb96
marutimuthu <marutimuthu@gmail.com>
2021-04-21 14:03:29 +0530

    testing reports

--- data.js
+++ data.js
@@ -3,8 +3,8 @@ const fetch = require('cross-fetch');
 const { exec } = require('child_process');
 // const CronJob = require('cron').CronJob;
 
-// const Gpio = require('onoff').Gpio;
-// const proxy = new Gpio(22, 'in', 'falling', { debounceTimeout: 10 });
+const Gpio = require('onoff').Gpio;
+const proxy = new Gpio(22, 'in', 'falling', { debounceTimeout: 10 });
 
 var host = "http://localhost";
 // var os = require("os");
@@ -1236,15 +1236,15 @@ var watchproxy = function () {
     writemachine();
     console.log("Watching proxy")
    
-    // proxy.watch((err, value) => {
-    //     if (err) {
-    //         throw err;
-    //     }
-    //     payload.data_number++;
-    //     console.log("DATA")
-    //     writeHistory();
-    //     writeAverage();
-    // });
+    proxy.watch((err, value) => {
+        if (err) {
+            throw err;
+        }
+        payload.data_number++;
+        console.log("DATA")
+        writeHistory();
+        writeAverage();
+    });
     // setTimeout(() => {
     //     setInterval(() => {
     //         payload.data_number++;
