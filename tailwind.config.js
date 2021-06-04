27b10575ffec5ad409f165a07b00fd9bf6e1e496
marutimuthu <marutimuthu@gmail.com>
2021-06-05 02:34:26 +0530

    added tailwind utilities

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
