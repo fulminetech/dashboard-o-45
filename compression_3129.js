2781010a4283af33e7c0dd59debe8b4563ebcee5
marutimuthu <marutimuthu@gmail.com>
2021-07-02 13:31:51 +0530

    compression

--- compression_3129.js
+++ compression_3129.js
@@ -1386,7 +1386,10 @@ var readinput = function () {
 var readoutput = function () {
     client.readCoils(output_address, 70)
         .then(function (output) {
-            // console.log("Output: ", output.data)
+            console.log("23", output.data[23])
+            console.log("18", output.data[18])
+            console.log("35", output.data[35])
+            console.log("38", output.data[38])
             payload.output = output.data;
             
             mbsState = MBS_STATE_GOOD_READ_OUTPUT;
