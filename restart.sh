f516666c23d5d7d1bb0f4be67f28a94c142ea03c
marutimuthu <marutimuthu@gmail.com>
2021-07-22 17:48:10 +0530

    update

--- restart.sh
+++ restart.sh
@@ -1,6 +1,6 @@
 #  const restart1Command = "pm2 reload stats_3128 && pm2 reload main_5000 && pm2 reload compression_3129"
-pm2 restart main_5000
+pm2 restart compression_3129
 sleep 2
 pm2 restart stats_3128 
 sleep 2
-pm2 restart compression_3129
+pm2 restart main_5000
